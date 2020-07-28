bff7501f96204c0d3d819b57f2f88246f8a28f38
Iconoclast209 <mrpresidentsmith@gmail.com>
2020-07-27 20:35:47 -0700

    44. Added Rigidbody and thrust.

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Rocket : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Moving Upwards");
+            rb.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.D))
